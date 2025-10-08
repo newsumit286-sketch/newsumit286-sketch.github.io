9d0740bec7c65b1f7ba20bd8273723c9993eb4af
newsumit286-sketch <newsumit286@gmail.com>
2025-10-08 22:21:31 +0530

    Update generate.py script

--- generate.py
+++ generate.py
@@ -1,1 +1,15 @@
+import os
+import google.generativeai as genai
 
+# Gemini API key lena (GitHub secret se)
+api_key = os.getenv("GEMINIAPIKEY")
+genai.configure(api_key=api_key)
+
+# Model initialize
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+# Simple test run
+prompt = "Hello Gemini! Is integration working?"
+response = model.generate_content(prompt)
+
+print(response.text)
